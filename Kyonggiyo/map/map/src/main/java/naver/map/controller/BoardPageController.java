2a01c5f4de0c3cbd8128f5cf695771fbf3f63a03
weonest <geonhee33@naver.com>
2023-01-09 21:05:20 +0900

    게시판 조회 추가

---
+++ Kyonggiyo/map/map/src/main/java/naver/map/controller/BoardPageController.java
@@ -0,0 +1,15 @@
+package naver.map.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardPageController {
+
+    @GetMapping("/list")
+    public String openBoardList() {
+        return "board/list";
+    }
+}
