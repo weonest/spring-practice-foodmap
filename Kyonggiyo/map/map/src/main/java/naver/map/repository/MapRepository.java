b1172cc4f4359c6fb1a53bb3eb367103f57da1b0
weonest <geonhee33@naver.com>
2023-05-10 23:07:02 +0900

    -dev: QueryDSL 테스트코드

--- Kyonggiyo/map/map/src/main/java/naver/map/repository/MapRepository.java
+++ Kyonggiyo/map/map/src/main/java/naver/map/repository/MapRepository.java
@@ -6,19 +6,19 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface MapRepository extends JpaRepository<Map, Long> {
+public interface MapRepository extends JpaRepository<Map, Long>, MapRepositoryCustom{
 
-    List<Map> findAll();
-
-    List<Map> findByCamp(int camp);
-
-    @Query("select m from Map m ORDER BY m.star DESC, m.id ASC")
-    List<Map> findAllByOrder();
-
-    @Query("select m from Map m where m.camp =:camp ORDER BY m.star DESC, m.id ASC")
-    List<Map> findWithCampOrder(int camp);
-
-    List<Map> findByNameContainingOrDesContainingOrSumContaining(String name, String des, String sum);
+//    List<Map> findAll();
+//
+//    List<Map> findByCamp(int camp);
+//
+//    @Query("select m from Map m ORDER BY m.star DESC, m.id ASC")
+//    List<Map> findAllByOrder();
+//
+//    @Query("select m from Map m where m.camp =:camp ORDER BY m.star DESC, m.id ASC")
+//    List<Map> findWithCampOrder(int camp);
+//
+//    List<Map> findByNameContainingOrDesContainingOrSumContaining(String name, String des, String sum);
 }
 
 
---
+++ Kyonggiyo/map/map/src/main/java/naver/map/repository/MapRepositoryCustom.java
@@ -0,0 +1,19 @@
+package naver.map.repository;
+
+import naver.map.domain.Map;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface MapRepositoryCustom {
+
+    List<Map> findAll();
+
+    List<Map> findByCamp(int camp);
+
+    List<Map> findAllByOrder();
+
+    List<Map> findWithCampOrder(int camp);
+
+    List<Map> findByNameContainingOrDesContainingOrSumContaining(String name, String des, String sum);
+}
